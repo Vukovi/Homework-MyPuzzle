de680685c17b1929f1d7562b891a75620ecc5966
Vukovi <Vuk@Users-MacBook-Pro.local>
2017-03-07 23:49:39 +0100

    a

--- domaci mojPuzzle/MojView.swift
+++ domaci mojPuzzle/MojView.swift
@@ -1,6 +1,6 @@
 //
 //  MojView.swift
-//  domaci mojPuzzle
+//  domaci mojPuzzle///
 //
 //  Created by Vuk on 6/22/16.
 //  Copyright © 2016 User. All rights reserved.
@@ -8,14 +8,31 @@
 
 import UIKit
 
-class MojView: UIView {
-
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+class MojView: UIImageView {
+    
+        //var slicica: UIImageView?
+        var lastLocation: CGPoint = CGPoint(x: 0,y: 0)
+        override init(image: UIImage?) {
+            super.init(image: image)
+            let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(MojView.detectPan(_:)))
+            self.addGestureRecognizer(panRecognizer)
+            self.isUserInteractionEnabled = true
+        }
+    
+        required init?(coder aDecoder: NSCoder) {
+            fatalError("Nije implementiran koder")
+        }
+        
+        func detectPan (_ recognizer: UIPanGestureRecognizer){
+            let translation = recognizer.translation(in: self.superview!)
+            print("Promena koordinata za \(translation.x), \(translation.y)")
+            self.center = CGPoint(x: lastLocation.x + translation.x, y: lastLocation.y + translation.y)
+        }
+    
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            self.superview?.bringSubview(toFront: self)
+            lastLocation = self.center
+        }
+        
 }
+
--- domaci mojPuzzle/ViewController.swift
+++ domaci mojPuzzle/ViewController.swift
@@ -10,16 +10,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    let teodosic = UIImage(named: "TEODOSIC.jpg")
+    var slikeSlagalice = [UIImage]()
+    //var slikeSlagalice = [CGImage]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        iseciSliku(teodosic!, kolone: 4, redovi: 3)
+        kreirajSveObjekte()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func iseciSliku (_ slika: UIImage, kolone: CGFloat, redovi: CGFloat) -> UIImage {
+       
+        var slikaSlagalice = CGImage?()
+        var slicicaSlagalice = UIImage?()
+        let sirinaSlike = slika.size.width
+        let visinaSlike = slika.size.height
+        let sirinaCelije = sirinaSlike / kolone
+        let visinaCelije = visinaSlike / redovi
+        let celija: CGFloat?
+        if sirinaCelije <= visinaCelije {
+            celija = sirinaCelije
+        }
+        else {
+            celija = visinaCelije
+        }
+        
+        
+        for i in 0...Int(kolone)-1{
+            for j in 0...Int(redovi)-1{
+                let kvadrat = CGRect(x: CGFloat(i) * celija!, y: CGFloat(j) * celija!, width: celija!, height: celija!)
+                slikaSlagalice = slika.cgImage.cropping(to: kvadrat)
+                slicicaSlagalice = UIImage(cgImage: slikaSlagalice!)
+                slikeSlagalice.append(slicicaSlagalice!)
+                //slikeSlagalice.append(slikaSlagalice!)
+                
+            }
+            
+        }
+        print(slikeSlagalice)
+        return slicicaSlagalice!
     }
-
+    
+    
+    let sirinaObjekta = 50
+    
+    func kreirajSveObjekte(){
+        
+        //sada ovim hocemo da smestimo sve kvadrate u prostor ekrana pomocu sledece f-je:
+        let insetSize = self.view.bounds.insetBy(dx: CGFloat(Int(sirinaObjekta)), dy: CGFloat(Int(sirinaObjekta))).size
+        
+        
+        for i in 0...slikeSlagalice.count-1{
+            
+            let pointX = CGFloat(UInt(arc4random() % UInt32(UInt(insetSize.width))))
+            let pointY = CGFloat(UInt(arc4random() % UInt32(UInt(insetSize.height))))
+            let noviFrejm = CGRect(x: pointX, y: pointY, width: CGFloat(sirinaObjekta), height: CGFloat(sirinaObjekta))
+            let slika = slikeSlagalice[i]
+            let novaSlika = MojView(image: slika)
+            novaSlika.frame = noviFrejm
+            novaSlika.contentMode = UIViewContentMode.scaleAspectFit
+            
+            view.addSubview(novaSlika)
+          
+        }
+    }
 
 }
 
